03f05e509d9a0420fa1a5b37cbf51eb2f478a49e
sagni <sagni@192.168.1.4>
2021-08-13 19:13:10 +0530

    Spring Boot JPA - Performed all CRUD Operations

---
+++ bootjpa/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into alien values (101,'Sagnik','Java');
+insert into alien values (102,'Abhishek','Python');
+insert into alien values (103,'Pravin','React');
+insert into alien values (104,'John','Node js');
+insert into alien values (105,'Amit','Golang');
